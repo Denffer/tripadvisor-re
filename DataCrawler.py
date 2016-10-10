39b2c084833e49be70d19d5966bc1d05b042a592
denffer <denffer18@gmail.com>
2016-10-10 11:36:02 +0800

    Version 3.0

--- DataCrawler.py
+++ DataCrawler.py
@@ -11,15 +11,16 @@ class DataCrawler:
 
     def __init__(self):
         """ Initialize Values """
-        self.url = "https://www.tripadvisor.com/Attraction_Review-g293916-d1996018-Reviews-Rattanakosin_Exhibition_Hall-Bangkok.html"
+        self.url = "https://www.tripadvisor.com/Attraction_Review-g293916-d4322781-Reviews-Train_night_market-Bangkok.html"
+        #self.url = "https://www.tripadvisor.com/Attraction_Review-g293916-d1996018-Reviews-Rattanakosin_Exhibition_Hall-Bangkok.html"
         #self.url = "https://www.tripadvisor.com/Attraction_Review-g293916-d2209608-Reviews-Wat_Bowonniwet_Vihara-Bangkok.html"
         #self.url = "https://www.tripadvisor.com/Attraction_Review-g293916-d311043-Reviews-Temple_of_the_Reclining_Buddha_Wat_Pho-Bangkok.html"
-        self.dst = 'data/attraction_list.json'
+        self.dst = 'data/attraction_35.json'
         self.current_page = 0
         self.last_page = 1
         self.first_entry = 1
         self.soup = ""
-        
+
         self.location = ""
         self.attraction_name = ""
         self.ranking = ""
@@ -28,13 +29,13 @@ class DataCrawler:
         self.review_count = ""
 
         self.review_info_list = []
-        
+
         # Activate Chrome Driver
         self.driver = webdriver.Chrome()
 
     def pause(self):
         """ pause for a few seconds """
-        time.sleep(random.randint(6,8))
+        time.sleep(random.randint(4,7))
 
     def crawl(self, url):
         """ crawl data from tripadvisor official website """
@@ -48,44 +49,33 @@ class DataCrawler:
             elif connection == 404:
                 print "Error:", connection
                 menu.append("Error_404")
-                #self.pause()
             else:
                 print "\nConnection Successful:", connection
 
-                # let drive load url & expand all 'more'
+                # let drive load url
                 self.driver.get(url)
-                    
 
                 if self.first_entry == 1:
-                    #WebDriverWait(self.driver, timeout=20).until(lambda x: x.find_element_by_class_name('ui_close_x'))
+                     # execute javascript to click on all 'more'
                     self.driver.execute_script("if (document.querySelector('.ui_close_x')) {document.querySelector('.ui_close_x').click()};")
                     self.driver.execute_script("document.querySelector('.ulBlueLinks').click();")
-                
-                
-                #self.pause()
-                
 
-                #self.driver.execute_script("if (document.querySelector('.ui_close_x')) {document.querySelector('.ui_close_x').click()}; var element = document.querySelectorAll('.moreLink')[0]; element.click();")
-                #self._wait_for_engine_started(self.driver)
+                    #self.pause()
+                else:
+                    pass
+
                 self.driver.execute_script("document.querySelector('.ulBlueLinks').click();")
-                #self.driver.execute_script("var elements = document.querySelectorAll('.moreLink');for (var i = 0; i < elements.length; i++) {elements[i].click();}");
-                #WebDriverWait(self.driver, 15).until(EC.presence_of_element_located(self.driver.find_element_by_class_name('dyn_full_review')))
-                #print "Page is ready!"
-                # Put page_source in beautiful soup
-                self.pause()
-                WebDriverWait(self.driver, timeout=25).until(lambda x: x.find_element_by_class_name('entry'))
 
+                # Put page_source in beautiful soup
+
+                #WebDriverWait(self.driver, timeout=10).until(lambda x: x.find_element_by_class_name('entry'))
+
+                self.pause()
                 soup = BeautifulSoup(self.driver.page_source, "html.parser")
 
-                f2 = open('data/soup.txt', 'w+')
-                f2.write(str(soup))
-                f2.close()
 
-                # get current page
-                self.current_page += 1
 
                 if self.first_entry == 1:
-                    self.pause()
                     # get last page
                     for a in soup.find("div", {"class": "pageNumbers"}).findAll("a"):
                         self.last_page = a.getText()
@@ -105,45 +95,40 @@ class DataCrawler:
                     # get location
                     self.location = soup.find("div", {"class": "slim_ranking"}).find("a").getText()[16:]
                     self.first_entry = 0
-                
-                #for div in soup.findAll("div", {"class": "innerBubble"}):
+                else:
+                    pass
+
+                # add 1 current page
+                self.current_page += 1
+
+                # get full review
                 for div in soup.findAll("div", {"class": "dyn_full_review"}):
                     title = div.find("span", {"class": "noQuotes"}).getText()
-                    #print "title:" + title
                     rating = div.find("div", {"class": "rating"}).find("img")['alt'][0]
-                    #print "rating:" + rating
                     review = div.find("div", {"class": "entry"}).find("p").getText().strip("\n")
                     print "review:" + review
                     self.review_info_list.append([title, rating, review])
-
 
                 if int(self.current_page) < int(self.last_page):
 
                     if "-Reviews-or" in url:
                         insert_position = url.find("-Reviews-or")
-                        next_url = url[:insert_position+11] + str(self.current_page) + "0-" + url[insert_position+14:]
-                        self.pause()
+                        next_url = url[:insert_position+11] + str(self.current_page*10) + "-" + url[insert_position+12+len(str(self.current_page*10)):]
                     else:
                         insert_position = url.find("-Reviews-")
-                        next_url = url[:insert_position+9] + "or" + str(self.current_page) + "0-" + url[insert_position+9:]
-                        self.pause()
+                        next_url = url[:insert_position+9] + "or" + str(self.current_page*10) + "-" + url[insert_position+9:]
 
                     sys.stdout.write("\rStatus: %s / %s"%(self.current_page, self.last_page))
                     sys.stdout.flush()
-                    
+
+                    #self.pause()
                     self.crawl(next_url)
                 else:
+                    print "Page error:", sys.exc_info()[0]
                     pass
         except:
             print "Unexpected error:", sys.exc_info()[0]
             raise
-
-    def _wait_for_engine_started(self, driver):
-        WebDriverWait(driver, 20).until(
-            lambda driver: self.driver.execute_script(
-                'return window.ourNameSpace && window.ourNameSpace.engine && window.ourNameSpace.engine.isStarted;'
-            )
-        )
 
     def render(self):
         """ put things in order and render json file """
@@ -165,7 +150,7 @@ class DataCrawler:
         rating_stats_dict["average"] = self.rating_stats[2]
         rating_stats_dict["poor"] = self.rating_stats[3]
         rating_stats_dict["terrible"] = self.rating_stats[4]
-         
+
         attraction_ordered_dict["rating_stats"] = NoIndent(rating_stats_dict)
 
         review_ordered_dict_list = []
@@ -179,7 +164,7 @@ class DataCrawler:
             review_ordered_dict["review"] = review_info[2]
             review_ordered_dict_list.append(review_ordered_dict)
         attraction_ordered_dict["reviews"] = review_ordered_dict_list
- 
+
         print "Writing data to:", self.dst
         f = open(self.dst, 'w+')
         f.write( json.dumps( attraction_ordered_dict, indent = 4, cls=NoIndentEncoder))
