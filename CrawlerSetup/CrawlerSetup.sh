82e975d5798e77bac9a01effb7129693b041882c
denffer <denffer18@gmail.com>
2016-10-16 20:04:17 +0800

    fix bugs

--- CrawlerSetup/CrawlerSetup.sh
+++ CrawlerSetup/CrawlerSetup.sh
@@ -1,5 +1,7 @@
 # for mac
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew reinstall python
+sudo easy_install pip
 sudo mv chromedriver /usr/local/bin/
 cd beautifulsoup4-4.1.0/
 sudo python setup.py install
-sudo pip install selenium
