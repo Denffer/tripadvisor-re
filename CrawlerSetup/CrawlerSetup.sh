b266d63b233b1b3505670b9ee416205f81f30ef2
denffer <denffer18@gmail.com>
2016-10-26 16:08:57 +0800

    mv -> cp

--- CrawlerSetup/CrawlerSetup.sh
+++ CrawlerSetup/CrawlerSetup.sh
@@ -2,6 +2,6 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew reinstall python
 sudo easy_install pip
-sudo mv chromedriver /usr/local/bin/
+sudo cp chromedriver /usr/local/bin/
 cd beautifulsoup4-4.1.0/
 sudo python setup.py install
