b149f715f83839db2cf3c28609ad716c53d7f176
denffer <denffer18@gmail.com>
2016-11-11 12:55:48 +0800

    Version 2.0 (First Commit)

---
+++ Word2Vec.py
@@ -0,0 +1,75 @@
+import sys, os, uuid, json, gensim, itertools
+
+class Word2Vec:
+    """ A python implementation on Word2Vector"""
+    def __init__(self):
+        """ initalize paths """
+        self.src = "data/word2vec_input/corpus.txt"
+        self.dst_uw = "data/coreProcess/unique_words.txt"
+        self.dst_v200 = "data/coreProcess/vectors200.txt"
+
+        self.verbose = 1
+
+    def get_corpus(self):
+        """ get reviews in corpus """
+        print "Loading data from:", self.src
+
+        with open(self.src) as f:
+            corpus = f.read()
+
+        #print corpus
+        return corpus
+
+    def get_sentences(self):
+        """ get a list of lists of words | E.g. sentences = [["sentence","one"], ["sentence","two"]] """
+        corpus = self.get_corpus()
+
+        sentences = []
+        for sentence in corpus:
+            sentences.append(sentence.split())
+
+        #print sentences
+        return sentences
+
+    def run_word2vec(self):
+        """ run word to vector """
+        sentences = self.get_sentences()
+
+        print '\n' + '-'*80
+        print "Running Word2Vec"
+        model = gensim.models.Word2Vec(sentences, min_count=3, size=200, window = 10, workers=4)
+        unique_words = list(model.vocab.keys())
+
+        vectors200 = []
+        for word in unique_words:
+            vectors200.append(model[word].tolist())
+
+        #print unique_words, vectors200
+        return unique_words, vectors200
+
+    def create_folder(self):
+        """ create folder (1) coreProcess_input """
+        dir1 = os.path.dirname("data/coreProcess/")
+        if not os.path.exists(dir1):   # if the directory does not exist
+            os.makedirs(dir1)          # create the directory
+
+    def render(self):
+        """ render into two files """
+        unique_words, vectors200 = self.run_word2vec()
+        self.create_folder()
+
+        print "-"*80
+        print "Writing data to", self.dst_uw
+        with open(self.dst_uw, 'w+') as f_uw:
+            for word in unique_words:
+                f_uw.write( word + "\n")
+
+        print "Writing data to", self.dst_v200
+        with open(self.dst_v200, 'w+') as f_v200:
+            for vector in vectors200:
+                f_v200.write(str(vector) + '\n')
+
+if __name__ == '__main__':
+    word2vec = Word2Vec()
+    word2vec.render()
+
