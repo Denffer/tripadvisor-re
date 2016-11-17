b77d0bf68aefda78d18deab08f6f1682805c6c80
denffer <denffer18@gmail.com>
2016-11-17 18:11:56 +0800

    Version 1.0 (First Commit)

---
+++ Glove.py
@@ -0,0 +1,376 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser
+import codecs, itertools, logging, os.path, msgpack, re, sys
+from collections import Counter
+from functools import partial
+from math import log
+import cPickle as pickle
+from random import shuffle
+import numpy as np
+from scipy import sparse
+
+logger = logging.getLogger("glove")
+dst_vocab = "data/vocab/"
+dst_cooccur = "data/cooccur/"
+
+def parse_args():
+    parser = ArgumentParser(description=('Build a GloVe vector-space model from the provided corpus'))
+    parser.add_argument('corpus', metavar='corpus_path', type=partial(codecs.open, encoding='utf-8'))
+
+    g_vocab = parser.add_argument_group('Vocabulary options')
+    g_vocab.add_argument('--vocab-path',
+                         help=('Path to vocabulary file. If this path '
+                               'exists, the vocabulary will be loaded '
+                               'from the file. If it does not exist, '
+                               'the vocabulary will be written to this '
+                               'file.'))
+
+    g_cooccur = parser.add_argument_group('Cooccurrence tracking options')
+    g_cooccur.add_argument('--cooccur-path',
+                           help=('Path to cooccurrence matrix file. If '
+                                 'this path exists, the matrix will be '
+                                 'loaded from the file. If it does not '
+                                 'exist, the matrix will be written to '
+                                 'this file.'))
+    g_cooccur.add_argument('-w', '--window-size', type=int, default=10,
+                           help=('Number of context words to track to '
+                                 'left and right of each word'))
+    g_cooccur.add_argument('--min-count', type=int, default=None,
+                           help=('Discard cooccurrence pairs where at '
+                                 'least one of the words occurs fewer '
+                                 'than this many times in the training '
+                                 'corpus'))
+
+    g_glove = parser.add_argument_group('GloVe options')
+    g_glove.add_argument('--vector-path',
+                         help=('Path to which to save computed word '
+                               'vectors'))
+    g_glove.add_argument('-s', '--vector-size', type=int, default=100,
+                         help=('Dimensionality of output word vectors'))
+    g_glove.add_argument('--iterations', type=int, default=25,
+                         help='Number of training iterations')
+    g_glove.add_argument('--learning-rate', type=float, default=0.05,
+                         help='Initial learning rate')
+    g_glove.add_argument('--save-often', action='store_true', default=False,
+                         help=('Save vectors after every training '
+                               'iteration'))
+
+    return parser.parse_args()
+
+def get_or_build(path, build_fn, *args, **kwargs):
+    """ Load from serialized form or build an object, saving the built object.
+    Remaining arguments are provided to `build_fn`.
+    """
+
+    save = False
+    obj = None
+
+    if path is not None and os.path.isfile(path):
+        with open(path, 'rb') as obj_f:
+            obj = msgpack.load(obj_f, use_list=False, encoding='utf-8')
+    else:
+        save = True
+
+    if obj is None:
+        obj = build_fn(*args, **kwargs)
+
+        if save and path is not None:
+            with open(path, 'wb') as obj_f:
+                msgpack.dump(obj, obj_f)
+
+    return obj
+
+def build_vocab(corpus):
+    """
+    Build a vocabulary with word frequencies for an entire corpus.
+
+    Returns a dictionary `w -> (i, f)`, mapping word strings to pairs of
+    word ID and word corpus frequency.
+    """
+
+    logger.info("Building vocab from corpus")
+
+    vocab = Counter()
+    for line in corpus:
+        tokens = line.strip().split()
+        vocab.update(tokens)
+
+    logger.info("Done building vocab from corpus.")
+
+    return {word: (i, freq) for i, (word, freq) in enumerate(vocab.iteritems())}
+
+def build_cooccur(vocab, corpus, window_size=5, min_count=None):
+    """
+    Build a word co-occurrence list for the given corpus.
+
+    This function is a tuple generator, where each element (representing
+    a cooccurrence pair) is of the form
+
+        (i_main, i_context, cooccurrence)
+
+    where `i_main` is the ID of the main word in the cooccurrence and
+    `i_context` is the ID of the context word, and `cooccurrence` is the
+    `X_{ij}` cooccurrence value as described in Pennington et al.
+    (2014).
+
+    If `min_count` is not `None`, cooccurrence pairs where either word
+    occurs in the corpus fewer than `min_count` times are ignored.
+    """
+
+    vocab_size = len(vocab)
+    id2word = dict((i, word) for word, (i, _) in vocab.iteritems())
+
+    # Collect cooccurrences internally as a sparse matrix for passable
+    # indexing speed; we'll convert into a list later
+    cooccurrences = sparse.lil_matrix((vocab_size, vocab_size),
+                                      dtype=np.float64)
+
+    for i, line in enumerate(corpus):
+        if i % 1 == 0:
+            logger.info("Building cooccurrence matrix: on line %i", i)
+
+        tokens = line.strip().split()
+        token_ids = [vocab[word][0] for word in tokens]
+
+        for center_i, center_id in enumerate(token_ids):
+            # Collect all word IDs in left window of center word
+            context_ids = token_ids[max(0, center_i - window_size) : center_i]
+            contexts_len = len(context_ids)
+
+            for left_i, left_id in enumerate(context_ids):
+                # Distance from center word
+                distance = contexts_len - left_i
+
+                # Weight by inverse of distance between words
+                increment = 1.0 / float(distance)
+
+                # Build co-occurrence matrix symmetrically (pretend we
+                # are calculating right contexts as well)
+                cooccurrences[center_id, left_id] += increment
+                cooccurrences[left_id, center_id] += increment
+    # Now yield our tuple sequence (dig into the LiL-matrix internals to
+    # quickly iterate through all nonzero cells)
+    for i, (row, data) in enumerate(itertools.izip(cooccurrences.rows,
+                                                   cooccurrences.data)):
+        if min_count is not None and vocab[id2word[i]][1] < min_count:
+            continue
+
+        for data_idx, j in enumerate(row):
+            if min_count is not None and vocab[id2word[j]][1] < min_count:
+                continue
+
+            yield i, j, data[data_idx]
+
+
+def run_iter(vocab, data, learning_rate=0.05, x_max=100, alpha=0.75):
+    """
+    Run a single iteration of GloVe training using the given
+    cooccurrence data and the previously computed weight vectors /
+    biases and accompanying gradient histories.
+
+    `data` is a pre-fetched data / weights list where each element is of
+    the form
+
+        (v_main, v_context,
+         b_main, b_context,
+         gradsq_W_main, gradsq_W_context,
+         gradsq_b_main, gradsq_b_context,
+         cooccurrence)
+
+    as produced by the `train_glove` function. Each element in this
+    tuple is an `ndarray` view into the data structure which contains
+    it.
+
+    See the `train_glove` function for information on the shapes of `W`,
+    `biases`, `gradient_squared`, `gradient_squared_biases` and how they
+    should be initialized.
+
+    The parameters `x_max`, `alpha` define our weighting function when
+    computing the cost for two word pairs; see the GloVe paper for more
+    details.
+
+    Returns the cost associated with the given weight assignments and
+    updates the weights by online AdaGrad in place.
+    """
+
+    global_cost = 0
+
+    # We want to iterate over data randomly so as not to unintentionally
+    # bias the word vector contents
+    shuffle(data)
+
+    for (v_main, v_context, b_main, b_context, gradsq_W_main, gradsq_W_context,
+         gradsq_b_main, gradsq_b_context, cooccurrence) in data:
+
+        weight = (cooccurrence / x_max) ** alpha if cooccurrence < x_max else 1
+
+        # Compute inner component of cost function, which is used in
+        # both overall cost calculation and in gradient calculation
+        cost_inner = (v_main.dot(v_context)
+                      + b_main[0] + b_context[0]
+                      - log(cooccurrence))
+
+        # Compute cost
+        cost = weight * (cost_inner ** 2)
+
+        # Add weighted cost to the global cost tracker
+        global_cost += 0.5 * cost
+
+        # Compute gradients for word vector terms.
+        # NB: `main_word` is only a view into `W` (not a copy), so our
+        # modifications here will affect the global weight matrix;
+        # likewise for context_word, biases, etc.
+        grad_main = weight * cost_inner * v_context
+        grad_context = weight * cost_inner * v_main
+
+        # Compute gradients for bias terms
+        grad_bias_main = weight * cost_inner
+        grad_bias_context = weight * cost_inner
+
+        # Now perform adaptive updates
+        v_main -= (learning_rate * grad_main / np.sqrt(gradsq_W_main))
+        v_context -= (learning_rate * grad_context / np.sqrt(gradsq_W_context))
+
+        b_main -= (learning_rate * grad_bias_main / np.sqrt(gradsq_b_main))
+        b_context -= (learning_rate * grad_bias_context / np.sqrt(
+                gradsq_b_context))
+
+        # Update squared gradient sums
+        gradsq_W_main += np.square(grad_main)
+        gradsq_W_context += np.square(grad_context)
+        gradsq_b_main += grad_bias_main ** 2
+        gradsq_b_context += grad_bias_context ** 2
+
+    return global_cost
+
+def train_glove(vocab, cooccurrences, iter_callback=None, vector_size=100, iterations=25, **kwargs):
+    """
+    Train GloVe vectors on the given generator `cooccurrences`, where
+    each element is of the form
+
+        (word_i_id, word_j_id, x_ij)
+
+    where `x_ij` is a cooccurrence value $X_{ij}$ as presented in the
+    matrix defined by `build_cooccur` and the Pennington et al. (2014)
+    paper itself.
+
+    If `iter_callback` is not `None`, the provided function will be
+    called after each iteration with the learned `W` matrix so far.
+
+    Keyword arguments are passed on to the iteration step function
+    `run_iter`.
+
+    Returns the computed word vector matrix `W`.
+    """
+
+    vocab_size = len(vocab)
+
+    # Word vector matrix. This matrix is (2V) * d, where N is the size
+    # of the corpus vocabulary and d is the dimensionality of the word
+    # vectors. All elements are initialized randomly in the range (-0.5,
+    # 0.5]. We build two word vectors for each word: one for the word as
+    # the main (center) word and one for the word as a context word.
+    #
+    # It is up to the client to decide what to do with the resulting two
+    # vectors. Pennington et al. (2014) suggest adding or averaging the
+    # two for each word, or discarding the context vectors.
+    W = (np.random.rand(vocab_size * 2, vector_size) - 0.5) / float(vector_size + 1)
+
+    # Bias terms, each associated with a single vector. An array of size
+    # $2V$, initialized randomly in the range (-0.5, 0.5].
+    biases = (np.random.rand(vocab_size * 2) - 0.5) / float(vector_size + 1)
+
+    # Training is done via adaptive gradient descent (AdaGrad). To make
+    # this work we need to store the sum of squares of all previous
+    # gradients.
+    #
+    # Like `W`, this matrix is (2V) * d.
+    #
+    # Initialize all squared gradient sums to 1 so that our initial
+    # adaptive learning rate is simply the global learning rate.
+    gradient_squared = np.ones((vocab_size * 2, vector_size),
+                               dtype=np.float64)
+
+    # Sum of squared gradients for the bias terms.
+    gradient_squared_biases = np.ones(vocab_size * 2, dtype=np.float64)
+
+    # Build a reusable list from the given cooccurrence generator,
+    # pre-fetching all necessary data.
+    #
+    # NB: These are all views into the actual data matrices, so updates
+    # to them will pass on to the real data structures
+    #
+    # (We even extract the single-element biases as slices so that we
+    # can use them as views)
+    data = [(W[i_main], W[i_context + vocab_size],
+             biases[i_main : i_main + 1],
+             biases[i_context + vocab_size : i_context + vocab_size + 1],
+             gradient_squared[i_main], gradient_squared[i_context + vocab_size],
+             gradient_squared_biases[i_main : i_main + 1],
+             gradient_squared_biases[i_context + vocab_size
+                                     : i_context + vocab_size + 1],
+             cooccurrence)
+            for i_main, i_context, cooccurrence in cooccurrences]
+
+    for i in range(iterations):
+        logger.info("\tBeginning iteration %i..", i)
+        cost = run_iter(vocab, data, **kwargs)
+        logger.info("\t\tDone (cost %f)", cost)
+
+        if iter_callback is not None:
+            iter_callback(W)
+
+    return W
+
+def save_model(W, path):
+    with open(path, 'wb') as vector_f:
+        pickle.dump(W, vector_f, protocol=2)
+
+    logger.info("Saved vectors to %s", path)
+
+def create_dirs():
+    """ create the directory if not exist"""
+    dir1 = os.path.dirname(dst_vocab)
+    dir2 = os.path.dirname(dst_cooccur)
+
+    if not os.path.exists(dir1):
+        os.makedirs(dir1)
+    if not os.path.exists(dir2):
+        os.makedirs(dir2)
+
+def main(arguments):
+
+    # get file name
+    filename = re.findall("[a-z]+\.txt", str(arguments))
+    print filename
+    filename = filename[0]
+
+    create_dirs()
+
+    corpus = arguments.corpus
+
+    logger.info("Fetching vocab..")
+    vocab = get_or_build(arguments.vocab_path, build_vocab, corpus)
+    logger.info("Vocab has %i elements.\n", len(vocab))
+
+    inv_vocab={}
+    with open(dst_vocab+"/"+filename, 'w') as fp:
+        for key, value in vocab.items():
+             fp.write('%s %s\n' % (value[0], key))
+             inv_vocab[value[0]]=key
+
+    #print inv_vocab
+
+    logger.info("Fetching cooccurrence list..")
+    corpus.seek(0)
+    cooccurrences = get_or_build(arguments.cooccur_path,
+                                 build_cooccur, vocab, corpus,
+                                 window_size=arguments.window_size,
+                                 min_count=arguments.min_count)
+
+    with open(dst_cooccur+"/"+filename, 'w') as fp:
+        fp.write('\n'.join('%s %s %s' % (inv_vocab[x[0]], inv_vocab[x[1]], x[2]) for x in cooccurrences))
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    main(parse_args())
