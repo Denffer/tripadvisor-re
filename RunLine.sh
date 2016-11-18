2577b0d1792b3226aa6d3d41f0d1cac4c66bb690
denffer <denffer18@gmail.com>
2016-11-18 12:23:32 +0800

    Version 1.0 (First Commit)

---
+++ RunLine.sh
@@ -0,0 +1,25 @@
+
+dir1="./data/line/vectors200/"
+dir2="./data/line/norm_vectors200/"
+
+if [ ! -d "$dir1" ]
+then
+    echo "Creating directory:\033[1m" $dir1 "\033[0m"
+    mkdir $dir1
+fi
+
+if [ ! -d "$dir2" ]
+then
+    echo "Creating directory:\033[1m" $dir2 "\033[0m"
+    mkdir $dir2
+fi
+    
+for i in data/line/cooccur/*.txt
+do
+    echo "Running Line (first-order) on:\033[1m" $i "\033[0m"
+    filename=$(echo $i | cut -d'/' -f 4)
+    echo $filename
+    ./Line/line -train $i -output data/line/vectors200/$filename -size 200 -order 1 -negative 5 -samples 10 -threads 10
+    ./Line/normalize -input data/line/vectors200/$filename -output data/line/norm_vectors200/$filename -binary 0
+done    
+
