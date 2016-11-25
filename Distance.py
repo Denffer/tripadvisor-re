0610a597b4598464dc3750d2d1253bb66b1a6612
denffer <denffer18@gmail.com>
2016-11-25 20:41:06 +0800

    Version 2.0 (Incorporate Dot product)

--- Distance.py
+++ Distance.py
@@ -44,7 +44,7 @@ class LexiconMaker2:
             self.unique_words[line[0]]=index
             self.vectors200.append([float(i) for i in line[1:-1]])
 
-            sys.stdout.write("\rStatus: %s / %s"%(index, vocab_size))
+            sys.stdout.write("\rStatus: %s / %s"%(index+1, vocab_size))
             sys.stdout.flush()
 
         f_src.close()
@@ -79,70 +79,117 @@ class LexiconMaker2:
 
         print "-"*70
 
-    def get_top_sentiment_words(self):
+    def get_top_cosine(self):
         """ calculate cosine distance get top** sentiment_words """
 
-        self.get_sentiment_statistics()
-
         print "Calculating Cosine Similarity between queries and every positive word"
-        positive_cosine_similarities_list = []
+        positive_cosine_similarity_list = []
         for query in self.queries:
-            cosine_similarities = []
+            cosine_similarity_list = []
             for index in xrange(len(self.positive)):
-                cosine_similarities.append(1-spatial.distance.cosine(self.vectors200[self.unique_words[query]], self.positive_vectors200[index]))
+                cosine_similarity_list.append(1-spatial.distance.cosine(self.vectors200[self.unique_words[query]], self.positive_vectors200[index]))
 
             print "Generating top" + str(self.topN) + " sentiment words with " + "\033[1m" + query + "\033[0m"
 
-            sorted_index = sorted(range(len(cosine_similarities)), key=lambda k: cosine_similarities[k], reverse=True)
+            sorted_index = sorted(range(len(cosine_similarity_list)), key=lambda k: cosine_similarity_list[k], reverse=True)
             word_dict_list = []
             for i, index in enumerate(sorted_index):
                 if i < self.topN:
                     positive_word =  self.positive[index]
-                    word_dict = {"word": positive_word, "cosine_similarity": cosine_similarities[index]}
+                    word_dict = {"word": positive_word, "cosine_similarity": cosine_similarity_list[index]}
                     word_dict_list.append(word_dict)
-                    #print positive_word, cosine_similarities[index]
-            positive_cosine_similarities_list.append({"query": query, "top10_positive_cosine_similarities": word_dict_list})
+                    #print positive_word, cosine_similarity_list[index]
+            positive_cosine_similarity_list.append({"query": query, "top10_positive_cosine_similarity": word_dict_list})
 
         print "-"*70
         print "Calculating Cosine Similarity between queries and every negative word"
-        negative_cosine_similarities_list = []
+        negative_cosine_similarity_list = []
         for query in self.queries:
-            cosine_similarities = []
+            cosine_similarity_list = []
             for index in xrange(len(self.negative)):
-                cosine_similarities.append(1-spatial.distance.cosine(self.vectors200[self.unique_words[query]], self.negative_vectors200[index]))
+                cosine_similarity_list.append(1-spatial.distance.cosine(self.vectors200[self.unique_words[query]], self.negative_vectors200[index]))
 
             print "Generating top" + str(self.topN) + " sentiment words with " + "\033[1m" + query + "\033[0m"
 
-            sorted_index = sorted(range(len(cosine_similarities)), key=lambda k: cosine_similarities[k], reverse=True)
+            sorted_index = sorted(range(len(cosine_similarity_list)), key=lambda k: cosine_similarity_list[k], reverse=True)
             word_dict_list = []
             for i, index in enumerate(sorted_index):
                 if i < self.topN:
                     negative_word =  self.negative[index]
-                    word_dict = {"word": negative_word, "cosine_similarity": cosine_similarities[index]}
+                    word_dict = {"word": negative_word, "cosine_similarity": cosine_similarity_list[index]}
                     word_dict_list.append(word_dict)
-                    #print positive_word, cosine_similarities[index]
-            negative_cosine_similarities_list.append({"query": query, "top10_negative_cosine_similarities": word_dict_list})
+                    #print positive_word, cosine_similarity_list[index]
+            negative_cosine_similarity_list.append({"query": query, "top10_negative_cosine_similarity": word_dict_list})
 
         print "-"*70
 
-        return positive_cosine_similarities_list, negative_cosine_similarities_list
+        return positive_cosine_similarity_list, negative_cosine_similarity_list
+
+    def get_top_dot(self):
+        """ calculate dot product get top** sentiment_words """
+
+        print "Calculating Dot Product between queries and every positive word"
+        positive_dot_product_list = []
+        for query in self.queries:
+            dot_product_list = []
+            for index in xrange(len(self.positive)):
+                dot_product_list.append(np.dot(self.vectors200[self.unique_words[query]], self.positive_vectors200[index]))
+
+            print "Generating top" + str(self.topN) + " sentiment words with " + "\033[1m" + query + "\033[0m"
+
+            sorted_index = sorted(range(len(dot_product_list)), key=lambda k: dot_product_list[k], reverse=True)
+            word_dict_list = []
+            for i, index in enumerate(sorted_index):
+                if i < self.topN:
+                    positive_word =  self.positive[index]
+                    word_dict = {"word": positive_word, "dot_product": dot_product_list[index]}
+                    word_dict_list.append(word_dict)
+                    #print positive_word, dot_product_list[index]
+            positive_dot_product_list.append({"query": query, "top10_positive_dot_product": word_dict_list})
+
+        print "-"*70
+        print "Calculating Dot Product between queries and every negative word"
+        negative_dot_product_list = []
+        for query in self.queries:
+            dot_product_list = []
+            for index in xrange(len(self.negative)):
+                dot_product_list.append(np.dot(self.vectors200[self.unique_words[query]], self.negative_vectors200[index]))
+
+            print "Generating top" + str(self.topN) + " sentiment words with " + "\033[1m" + query + "\033[0m"
+
+            sorted_index = sorted(range(len(dot_product_list)), key=lambda k: dot_product_list[k], reverse=True)
+            word_dict_list = []
+            for i, index in enumerate(sorted_index):
+                if i < self.topN:
+                    negative_word =  self.negative[index]
+                    word_dict = {"word": negative_word, "dot_product": dot_product_list[index]}
+                    word_dict_list.append(word_dict)
+                    #print positive_word, cosine_similarity[index]
+            negative_dot_product_list.append({"query": query, "top10_negative_dot_product": word_dict_list})
+
+        print "-"*70
+
+        return positive_dot_product_list, negative_dot_product_list
 
     def render(self):
         """ save every cosine_list for top1~5 as json file"""
         self.get_source()
-        positive, negative = self.get_top_sentiment_words()
+        self.get_sentiment_statistics()
+        positive_cosine_similarity_list, negative_cosine_similarity_list = self.get_top_cosine()
+        positive_dot_product_list, negative_dot_product_list = self.get_top_dot()
 
-        print "Putting both cosine_similarities in order"
+        print "Putting both cosine_similarity and dot_product in order"
         #lexicon_ordered_dict = OrderedDict()
 
-        both_ordered_dict_list = []
-        for p_query, n_query in zip(positive, negative):
+        entire_ordered_dict_list = []
+        for p_cosine_query, n_cosine_query, p_dot_query, n_dot_query in zip(positive_cosine_similarity_list, negative_cosine_similarity_list, positive_dot_product_list, negative_dot_product_list):
             query_ordered_dict = OrderedDict()
-            query_ordered_dict["query"] = p_query["query"] # n_query will also fit
+            query_ordered_dict["query"] = p_cosine_query["query"] # n_cosine_query will also fit
 
+            # positive cosine
             positive_cosine_ordered_dict_list = []
             index = 0
-            for p_cosine_dict in p_query["top10_positive_cosine_similarities"]:
+            for p_cosine_dict in p_cosine_query["top10_positive_cosine_similarity"]:
 
                 index += 1
                 p_word_dict = OrderedDict()
@@ -155,11 +202,12 @@ class LexiconMaker2:
                 p_word_dict["polarity"] = p_cosine_dict["word"]["polarity"]
                 positive_cosine_ordered_dict_list.append(NoIndent(p_word_dict))
 
-            query_ordered_dict["top10_positive"] = positive_cosine_ordered_dict_list
+            query_ordered_dict["top10_positive_cosine_similarity"] = positive_cosine_ordered_dict_list
 
+            # negative cosine
             negative_cosine_ordered_dict_list = []
             index = 0
-            for n_cosine_dict in n_query["top10_negative_cosine_similarities"]:
+            for n_cosine_dict in n_cosine_query["top10_negative_cosine_similarity"]:
 
                 index += 1
                 n_word_dict = OrderedDict()
@@ -172,42 +220,50 @@ class LexiconMaker2:
                 n_word_dict["polarity"] = n_cosine_dict["word"]["polarity"]
                 negative_cosine_ordered_dict_list.append(NoIndent(n_word_dict))
 
-            query_ordered_dict["top10_negative"] = negative_cosine_ordered_dict_list
-            both_ordered_dict_list.append(query_ordered_dict)
+            query_ordered_dict["top10_negative_cosine_similarity"] = negative_cosine_ordered_dict_list
 
-            #  cosine_ordered_dict_list = []
-            #  index = 0
-            #  for p_cosine_dict, n_cosine_dict in zip(p_query["top10_positive_cosine_similarities"], n_query["top10_negative_cosine_similarities"]):
-            #      cosine_ordered_dict = OrderedDict()
-            #
-            #      index += 1
-            #      p_word_dict = OrderedDict()
-            #      p_word_dict["cosine_similarity"] = p_cosine_dict["cosine_similarity"]
-            #      p_word_dict["index"] = index
-            #      p_word_dict["count"] = p_cosine_dict["word"]["count"]
-            #      p_word_dict["stemmed_word"] = p_cosine_dict["word"]["stemmed_word"]
-            #      p_word_dict["word"] = p_cosine_dict["word"]["word"]
-            #      p_word_dict["strength"] = p_cosine_dict["word"]["strength"]
-            #      p_word_dict["polarity"] = p_cosine_dict["word"]["polarity"]
-            #      cosine_ordered_dict["positive_sentiment_word"] = NoIndent(p_word_dict)
-            #
-            #      n_word_dict = OrderedDict()
-            #      n_word_dict["cosine_similarity"] = n_cosine_dict["cosine_similarity"]
-            #      n_word_dict["index"] = index
-            #      n_word_dict["count"] = n_cosine_dict["word"]["count"]
-            #      n_word_dict["stemmed_word"] = n_cosine_dict["word"]["stemmed_word"]
-            #      n_word_dict["word"] = n_cosine_dict["word"]["word"]
-            #      n_word_dict["strength"] = n_cosine_dict["word"]["strength"]
-            #      n_word_dict["polarity"] = n_cosine_dict["word"]["polarity"]
-            #      cosine_ordered_dict["negative_sentiment_word"] = NoIndent(n_word_dict)
-            #
-            #      cosine_ordered_dict_list.append(cosine_ordered_dict)
-            #
-            #  query_ordered_dict["top10_cosine_similiarity"] = cosine_ordered_dict_list
-            #positive_ordered_dict_list.append(query_ordered_dict)
+            # positive dot
+            positive_dot_ordered_dict_list = []
+            index = 0
+            for p_dot_dict in p_dot_query["top10_positive_dot_product"]:
+
+                index += 1
+                p_word_dict = OrderedDict()
+                p_word_dict["dot_product"] = p_dot_dict["dot_product"]
+                p_word_dict["index"] = index
+                p_word_dict["count"] = p_dot_dict["word"]["count"]
+                p_word_dict["stemmed_word"] = p_dot_dict["word"]["stemmed_word"]
+                p_word_dict["word"] = p_dot_dict["word"]["word"]
+                p_word_dict["strength"] = p_dot_dict["word"]["strength"]
+                p_word_dict["polarity"] = p_dot_dict["word"]["polarity"]
+                positive_dot_ordered_dict_list.append(NoIndent(p_word_dict))
+
+            query_ordered_dict["top10_positive_dot_product"] = positive_dot_ordered_dict_list
+
+            # negative dot
+            negative_dot_ordered_dict_list = []
+            index = 0
+            for n_dot_dict in n_dot_query["top10_negative_dot_product"]:
+
+                index += 1
+                n_word_dict = OrderedDict()
+                n_word_dict["dot_product"] = n_dot_dict["dot_product"]
+                n_word_dict["index"] = index
+                n_word_dict["count"] = n_dot_dict["word"]["count"]
+                n_word_dict["stemmed_word"] = n_dot_dict["word"]["stemmed_word"]
+                n_word_dict["word"] = n_dot_dict["word"]["word"]
+                n_word_dict["strength"] = n_dot_dict["word"]["strength"]
+                n_word_dict["polarity"] = n_dot_dict["word"]["polarity"]
+                negative_dot_ordered_dict_list.append(NoIndent(n_word_dict))
+
+            query_ordered_dict["top10_negative_dot_product"] = negative_dot_ordered_dict_list
+            
+            # finish one query
+            entire_ordered_dict_list.append(query_ordered_dict)
 
 	f = open(self.dst, "w")
-        f.write(json.dumps(both_ordered_dict_list, indent = 4, cls=NoIndentEncoder))
+        f.write(json.dumps(entire_ordered_dict_list, indent = 4, cls=NoIndentEncoder))
+        print "Done"
 
 class NoIndent(object):
     def __init__(self, value):
