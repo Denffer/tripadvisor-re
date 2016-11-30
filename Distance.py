c26b6f95e119474658400734bec372492dc2392a
denffer <denffer18@gmail.com>
2016-11-30 21:11:53 +0800

    Version 5.1 (add self.cosine_lambda)

--- Distance.py
+++ Distance.py
@@ -20,7 +20,7 @@ class Distance:
         self.dst_r = "./data/ranking/" + self.filename + ".json"
 
         self.topN = 10
-        self.lambda = 0.5
+        self.cosine_lambda = 0.5
         self.queries = []
         #self.queries = {"star_1":1, "star_2":2, "star_3":3, "star_4":4, "star_5":5}
         self.positive_statistics = []
@@ -142,7 +142,7 @@ class Distance:
 
             topN_cos_sim_list = [float(word_dict["cos_sim"]) for word_dict in word_dict_list]
             # calculate the score out of topN cosine similarity
-            cos_score =  (self.lambda)(max(topN_cos_sim_list)) + (1-self.lambda)(sum(topN_cos_sim_list) / len(topN_cos_sim_list))
+            cos_score =  (self.cosine_lambda)(max(topN_cos_sim_list)) + (1-self.cosine_lambda)(sum(topN_cos_sim_list) / len(topN_cos_sim_list))
             positive_cosine_topN.append({"query": query, "positive_topN_cosine_similarity": word_dict_list, "cos_score": cos_score})
 
         print "-"*70
@@ -165,7 +165,7 @@ class Distance:
 
             topN_cos_sim_list = [float(word_dict["cos_sim"]) for word_dict in word_dict_list]
             # calculate the score out of topN cosine similarity
-            cos_score =  (self.lambda)(max(topN_cos_sim_list)) + (1-self.lambda)(sum(topN_cos_sim_list) / len(topN_cos_sim_list))
+            cos_score =  (self.cosine_lambda)(max(topN_cos_sim_list)) + (1-self.cosine_lambda)(sum(topN_cos_sim_list) / len(topN_cos_sim_list))
             negative_cosine_topN.append({"query": query, "negative_topN_cosine_similarity": word_dict_list, "cos_score": cos_score})
 
         print "-"*70
