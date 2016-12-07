296c79f000dbe1c86abe138b01a3df68fa375320
denffer <denffer18@gmail.com>
2016-12-07 14:05:17 +0800

    Remove ignored files

--- Line/Compile.sh
+++
@@ -1,2 +0,0 @@
-g++ -lm -pthread -Ofast -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result line.cpp -o line -lgsl -lm -lgslcblas
-g++ -lm -pthread -Ofast -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result normalize.cpp -o normalize
