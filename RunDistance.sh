c1bc1c415b9a90cab8f4e33e056b583c0cff76df
denffer <denffer18@gmail.com>
2016-12-16 16:11:32 +0800

    Version 2.0 (begin 0.0 step 0.1 end 1)

--- RunDistance.sh
+++ RunDistance.sh
@@ -1,11 +1,12 @@
 #!/bin/zsh
 for i in data/line/vectors200/Bangkok.txt 
 do
-    for j in $(seq 1 10)
+    for j in $(seq 0.0 0.1 1)
     do 
-        ts -n -f sh -c "python Distance.py $i $j" &
+        python Distance.py $i $j
+        #ts -n -f sh -c "python Distance.py $i $j" &
     done
 done
-ts -S 10
+#ts -S 10
 
 
