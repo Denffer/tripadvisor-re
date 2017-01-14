16420545ff879a1a4eeb563729e12c0b9f2a3902
denffer <denffer18@gmail.com>
2017-01-14 08:32:20 +0800

    sum score not divide

--- Distance.py
+++ Distance.py
@@ -1,4 +1,8 @@
-import sys, os, json, uuid, re, linecache
+try:
+    import simplejson as json
+except ImportError:
+    import json
+import sys, os, uuid, re, linecache
 from sklearn.metrics.pairwise import cosine_similarity
 from collections import OrderedDict
 import numpy as np
@@ -13,22 +17,25 @@ class Distance:
 
     def __init__(self, argv1, argv2):
         self.src = argv1
-        self.tuning_lambda = float(argv2)
+        #self.tuning_lambda = float(argv2)
+        self.threshold = float(argv2)
         self.src_lexicon = "data/lexicon/enhanced_lexicon.json"
         self.filename = re.search("([A-Za-z|.]+\_*[A-Za-z|.]+\_*[A-Za-z|.]+)\.txt", self.src).group(1)
         self.src_fr = "data/frontend_reviews/" + self.filename + "/"
 
         self.verbose = 1
-        self.dst_dc = "data/distance/" + self.filename + "/" + self.filename + "-lambda" + str(float(argv2)) + ".json"
+        #self.dst_dc = "data/distance/" + self.filename + "/" + self.filename + "-lambda" + str(float(argv2)) + ".json"
+        self.dst_dc = "data/distance/" + self.filename + "/" + self.filename + "-threshold" + str(float(argv2)) + ".json"
         #self.dst_dd = "data/distance/" + self.filename + "/dot/" + self.filename + "-lambda" + str(float(argv2)) + ".json"
-        self.dst_rc = "data/ranking/" + self.filename + "/" + self.filename + "-lambda" + str(float(argv2)) + ".json"
+        #self.dst_rc = "data/ranking/" + self.filename + "/" + self.filename + "-lambda" + str(float(argv2)) + ".json"
+        self.dst_rc = "data/ranking/" + self.filename + "/" + self.filename + "-threshold" + str(float(argv2)) + ".json"
         #self.dst_rd = "data/ranking/" + self.filename + "/dot/" + self.filename + "-lambda" + str(float(argv2)) + ".json"
-        self.pn_flag = 1
+        self.flag = 1
         self.positive_minus_negative_flag = 0
         self.cosine_flag = 1
         self.dot_flag = 0
 
-        self.topN = 300
+        self.topN = 500
         self.topN_max = 5
         self.queries = []
         self.extreme_positive, self.strong_positive, self.moderate_positive = [], [], []
@@ -221,20 +228,23 @@ class Distance:
                     moderate_word_dict = {"word": self.moderate_positive[index], "cos_sim": moderate_cos_sim_list[index]}
                     moderate_word_dict_list.append(moderate_word_dict)
 
-            extreme_topN_cos_sim_list = [float(extreme_word_dict["cos_sim"]) for extreme_word_dict in extreme_word_dict_list]
-            strong_topN_cos_sim_list = [float(strong_word_dict["cos_sim"]) for strong_word_dict in strong_word_dict_list]
-            moderate_topN_cos_sim_list = [float(moderate_word_dict["cos_sim"]) for moderate_word_dict in moderate_word_dict_list]
+            extreme_topN_cos_sim_list = [float(extreme_word_dict["cos_sim"]) for extreme_word_dict in extreme_word_dict_list if float(extreme_word_dict["cos_sim"]) >= self.threshold]
+            strong_topN_cos_sim_list = [float(strong_word_dict["cos_sim"]) for strong_word_dict in strong_word_dict_list if float(strong_word_dict["cos_sim"]) >= self.threshold]
+            moderate_topN_cos_sim_list = [float(moderate_word_dict["cos_sim"]) for moderate_word_dict in moderate_word_dict_list if float(moderate_word_dict["cos_sim"]) >= self.threshold]
 
             # calculate the score out of topN cosine similarity
             #cos_score =  self.tuning_lambda * max(topN_cos_sim_list) + (1-self.tuning_lambda) * sum(topN_cos_sim_list) / len(topN_cos_sim_list)
-            extreme_cos_score =  self.tuning_lambda * sum(extreme_topN_cos_sim_list[:self.topN_max])/len(extreme_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(extreme_topN_cos_sim_list)/len(extreme_topN_cos_sim_list)
-            strong_cos_score =  self.tuning_lambda * sum(strong_topN_cos_sim_list[:self.topN_max])/len(strong_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(strong_topN_cos_sim_list)/len(strong_topN_cos_sim_list)
-            moderate_cos_score =  self.tuning_lambda * sum(moderate_topN_cos_sim_list[:self.topN_max])/len(moderate_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(moderate_topN_cos_sim_list)/len(moderate_topN_cos_sim_list)
+            extreme_cos_score = sum(extreme_topN_cos_sim_list)
+            strong_cos_score = sum(strong_topN_cos_sim_list)
+            moderate_cos_score = sum(moderate_topN_cos_sim_list)
+            #  extreme_cos_score =  self.tuning_lambda * sum(extreme_topN_cos_sim_list[:self.topN_max])/len(extreme_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(extreme_topN_cos_sim_list)/len(extreme_topN_cos_sim_list)
+            #  strong_cos_score =  self.tuning_lambda * sum(strong_topN_cos_sim_list[:self.topN_max])/len(strong_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(strong_topN_cos_sim_list)/len(strong_topN_cos_sim_list)
+            #  moderate_cos_score =  self.tuning_lambda * sum(moderate_topN_cos_sim_list[:self.topN_max])/len(moderate_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(moderate_topN_cos_sim_list)/len(moderate_topN_cos_sim_list)
 
             # generate cosine score
-            if self.pn_flag == 3:
-                cos_score = extreme_cos_score * 1 + strong_cos_score * 0.5 + moderate_cos_score * 0.3
-            if self.pn_flag == 1:
+            if self.flag == 3:
+                cos_score = extreme_cos_score + strong_cos_score + moderate_cos_score
+            if self.flag == 1:
                 cos_score = extreme_cos_score
             print "positive cosine score:", cos_score
 
@@ -283,20 +293,24 @@ class Distance:
                     moderate_word_dict = {"word": self.moderate_negative[index], "cos_sim": moderate_cos_sim_list[index]}
                     moderate_word_dict_list.append(moderate_word_dict)
 
-            extreme_topN_cos_sim_list = [float(extreme_word_dict["cos_sim"]) for extreme_word_dict in extreme_word_dict_list]
-            strong_topN_cos_sim_list = [float(strong_word_dict["cos_sim"]) for strong_word_dict in strong_word_dict_list]
-            moderate_topN_cos_sim_list = [float(moderate_word_dict["cos_sim"]) for moderate_word_dict in moderate_word_dict_list]
+            extreme_topN_cos_sim_list = [float(extreme_word_dict["cos_sim"]) for extreme_word_dict in extreme_word_dict_list if float(extreme_word_dict["cos_sim"]) >= self.threshold]
+            strong_topN_cos_sim_list = [float(strong_word_dict["cos_sim"]) for strong_word_dict in strong_word_dict_list if float(strong_word_dict["cos_sim"]) >= self.threshold]
+            moderate_topN_cos_sim_list = [float(moderate_word_dict["cos_sim"]) for moderate_word_dict in moderate_word_dict_list if float(moderate_word_dict["cos_sim"]) >= self.threshold]
 
             # calculate the score out of topN cosine similarity
+
             #cos_score =  self.tuning_lambda * max(topN_cos_sim_list) + (1-self.tuning_lambda) * sum(topN_cos_sim_list) / len(topN_cos_sim_list)
-            extreme_cos_score =  self.tuning_lambda * sum(extreme_topN_cos_sim_list[:self.topN_max])/len(extreme_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(extreme_topN_cos_sim_list)/len(extreme_topN_cos_sim_list)
-            strong_cos_score =  self.tuning_lambda * sum(strong_topN_cos_sim_list[:self.topN_max])/len(strong_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(strong_topN_cos_sim_list)/len(strong_topN_cos_sim_list)
-            moderate_cos_score =  self.tuning_lambda * sum(moderate_topN_cos_sim_list[:self.topN_max])/len(moderate_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(moderate_topN_cos_sim_list)/len(moderate_topN_cos_sim_list)
+            extreme_cos_score = sum(extreme_topN_cos_sim_list)
+            strong_cos_score = sum(strong_topN_cos_sim_list)
+            moderate_cos_score = sum(moderate_topN_cos_sim_list)
+            # extreme_cos_score =  self.tuning_lambda * sum(extreme_topN_cos_sim_list[:self.topN_max])/len(extreme_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(extreme_topN_cos_sim_list)/len(extreme_topN_cos_sim_list)
+            # strong_cos_score =  self.tuning_lambda * sum(strong_topN_cos_sim_list[:self.topN_max])/len(strong_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(strong_topN_cos_sim_list)/len(strong_topN_cos_sim_list)
+            # moderate_cos_score =  self.tuning_lambda * sum(moderate_topN_cos_sim_list[:self.topN_max])/len(moderate_topN_cos_sim_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(moderate_topN_cos_sim_list)/len(moderate_topN_cos_sim_list)
 
             # generate cosine score
-            if self.pn_flag == 3:
-                cos_score = extreme_cos_score * 1 + strong_cos_score * 0.5 + moderate_cos_score * 0.3
-            if self.pn_flag == 1:
+            if self.flag == 3:
+                cos_score = extreme_cos_score + strong_cos_score + moderate_cos_score
+            if self.flag == 1:
                 cos_score = extreme_cos_score
             print "negative cosine score:", cos_score
 
@@ -364,9 +378,9 @@ class Distance:
             moderate_dot_prod =  self.tuning_lambda * sum(moderate_topN_dot_prod_list[:self.topN_max])/len(moderate_topN_dot_prod_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(moderate_topN_dot_prod_list)/len(moderate_topN_dot_prod_list)
 
             # generate dot score
-            if self.pn_flag == 3:
+            if self.flag == 3:
                 dot_score = extreme_dot_prod * 1 + strong_dot_prod * 0.5 + moderate_dot_prod * 0.3
-            if self.pn_flag == 1:
+            if self.flag == 1:
                 dot_score = extreme_dot_prod
             print "positive dot score:", dot_score
 
@@ -426,9 +440,9 @@ class Distance:
             moderate_dot_prod =  self.tuning_lambda * sum(moderate_topN_dot_prod_list[:self.topN_max])/len(moderate_topN_dot_prod_list[:self.topN_max]) + (1-self.tuning_lambda) * sum(moderate_topN_dot_prod_list)/len(moderate_topN_dot_prod_list)
 
             # generate dot score
-            if self.pn_flag == 3:
+            if self.flag == 3:
                 dot_score = extreme_dot_prod * 1 + strong_dot_prod * 0.5 + moderate_dot_prod * 0.3
-            if self.pn_flag == 1:
+            if self.flag == 1:
                 dot_score = extreme_dot_prod
             print "Negative dot score:", dot_score
 
@@ -482,7 +496,8 @@ class Distance:
                 cnt += 1
                 query_ordered_dict = OrderedDict()
                 query_ordered_dict["query"] = p_cos_word_dict["query"]
-                query_ordered_dict["lambda"] = self.tuning_lambda
+                #query_ordered_dict["lambda"] = self.tuning_lambda
+                query_ordered_dict["cosine_threshold"] = self.threshold
 
                 # (1) extreme positive cosine
                 extreme_positive_cosine_word_dict_list = []
@@ -733,9 +748,10 @@ class Distance:
         # (1) cos_ranking
         if self.cosine_flag:
             location_ordered_dict = OrderedDict()
-            location_ordered_dict['lambda'] = self.tuning_lambda
+            #location_ordered_dict['lambda'] = self.tuning_lambda
+            location_ordered_dict['threshold'] = self.threshold
             location_ordered_dict['topN'] = self.topN
-            location_ordered_dict['topN_max'] = self.topN_max
+            #location_ordered_dict['topN_max'] = self.topN_max
             if self.verbose:
                 print "Ranking queries according to cosine score"
             score_list = []
