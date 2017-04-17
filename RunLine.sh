eb7362a7ead2972559876f1749b35c1dd79c4385
denffer <denffer18@gmail.com>
2017-04-17 17:29:26 +0800

    change dir line to glove

--- RunLine.sh
+++ RunLine.sh
@@ -13,13 +13,13 @@ then
     echo "Creating directory:\033[1m" $dir2 "\033[0m"
     mkdir $dir2
 fi
-for i in data/line/cooccur/marrakech.txt
+for i in data/glove/cooccur/
     
 do
     echo "Running Line (first-order) on:\033[1m" $i "\033[0m"
     filename=$(echo $i | cut -d'/' -f 4)
     echo $filename
-    ./Line/line -train $i -output data/line/vectors200/$filename -size 200 -order 1 -negative 30 -samples 15 -threads 2
+    ./Line/line -train $i -output data/line/vectors200/$filename -size 200 -order 1 -negative 20 -samples 25 -threads 2
     ./Line/normalize -input data/line/vectors200/$filename -output data/line/norm_vectors200/$filename -binary 0
     echo "-------------------------------------------"
 done    
